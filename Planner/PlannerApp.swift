85b30ae0535ee8c52d397c7761b862fed7becc77
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2024-09-03 19:56:52 +0300

    feat: project created

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by İbrahim Aykut BAŞ on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
